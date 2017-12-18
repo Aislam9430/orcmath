1ff5edabfc0726ed46e4163769b72c40bbb160b3
BT_1N3_12 <BT_1N3_12@BT_1N3_12.bktech.local>
2017-12-18 15:35:49 -0500

    dss

--- OrcMathGUI/src/guiPlayer/CatalogMaker.java
+++ OrcMathGUI/src/guiPlayer/CatalogMaker.java
@@ -1,8 +1,14 @@
 package guiPlayer;
 
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class CatalogMaker 
 {
@@ -15,6 +21,7 @@ public class CatalogMaker
 		list.add(new CDs("Saturation III","Brockhampton",11,21231,new Date()));
 		list.add(new CDs("No One Really Dies","N.E.R.D",11,24234,new Date()));
 		Scanner n = new Scanner(System.in);
+		
 	}	
 	
 	public String getCSVContent()
@@ -26,6 +33,59 @@ public class CatalogMaker
 		}
 		return data; 
 	}
+	private void testSaveContent(String fileName) 
+	{
+		try
+		{   
+			FileWriter fw=new FileWriter(fileName);    
+			for(CDs s: list)
+			{
+				fw.write(s + "\n");
+			}
+			fw.close();    
+		}
+		catch(IOException e)
+		{
+			System.out.println("An IOException was thrown. \nCheck to see that the directory where you tried to save the file actually exists.");
+		}
+	}
+	private List<String> testFileLoading() 
+	{
+		Scanner in = new Scanner(System.in);
+		String fileName = "";
+		//use this boolean to control the while loop. The user should have multiple chances to enter a correct filename
+		boolean opened = false;
+		while(!opened)
+		{
+			try 
+			{
+				System.out.println("Enter a file to open");
+				fileName = in.nextLine();
+				FileReader fileReader = new FileReader(new File(fileName));
+				String line = "";
+				//a BufferedReader enables us to read the file one line at a time
+				BufferedReader br = new BufferedReader(fileReader);
+				while ((line = br.readLine()) != null) 
+				{
+					
+					String[] param = line.split(",");
+					
+					
+					 
+				}
+				br.close();
+				opened = true;
+			}
+			catch (IOException e) 
+			{
+			System.out.println("The file name you specified does not exist.");
+			}
+		}
+		//close the Scanner
+		in.close();
+		return content;
+	}
+	
 	public void addNewItem(String name,String artist,int price,int isbn)
 	{
 		
