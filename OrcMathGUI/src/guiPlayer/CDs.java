fc1868bc3f4ca13072f1c1ed97395940cb2679ca
BT_1N3_12 <BT_1N3_12@BT_1N3_12.bktech.local>
2017-12-20 14:18:22 -0500

    adadasas

---
+++ OrcMathGUI/src/guiPlayer/CDs.java
@@ -0,0 +1,35 @@
+package guiPlayer;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.util.Date;
+
+import guiTeacher.components.AnimatedComponent;
+import guiTeacher.components.Component;
+
+public class CDs extends AnimatedComponent 
+{
+	private String name;
+	private String artist;
+	private int price;
+	private int isbn;
+	private Date releaseDate;
+
+	public CDs(String name,String artist,int price,int isbn,Date releaseDate)
+	{
+		super(0,0,300,300);
+		this.name = name;
+		this.artist = artist;
+		this.price = price; 
+		this.isbn = isbn;
+		this.releaseDate = releaseDate;
+	}
+
+	public String toString()
+	{
+		return name + "," + artist + "," + price + "," + isbn + "," + releaseDate;
+	}
+	
+	
+
+}
