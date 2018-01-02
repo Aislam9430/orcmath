067b9a7c23cd3f0573b3d00ec4cf28cf83dc866a
BT_1N3_12 <BT_1N3_12@BT_1N3_12.bktech.local>
2018-01-02 15:33:33 -0500

    dffd

--- OrcMathGUI/src/myStuffOnly/CustomButton.java
+++ OrcMathGUI/src/myStuffOnly/CustomButton.java
@@ -9,23 +9,40 @@ import guiPlayer.CustomLabel;
 
 public class CustomButton extends Button implements CustomLabel
 {
-
+	private String string1;
+	private String string2;
+	
 	public CustomButton(int x, int y, int w, int h, String text, Color color, Action action) 
 	{
 		super(x, y, w, h, text, color, action);
-		// TODO Auto-generated constructor stub
 	}
 
 	public CustomButton(int x, int y, int w, int h, String text, Action action) 
 	{
 		super(x, y, w, h, text, action);
-		// TODO Auto-generated constructor stub
 	}
 	public void update(Graphics2D g)
 	{
 		
 	}
 	public void drawButton(Graphics2D g, boolean hover)
+	{
+		g.setColor(new Color(0,0,0));
+		g.drawString(string1, 150, 150);
+		g.drawString(string2, 0, 150);
+	}
+
+	public void updateString1(String string) 
+	{
+		this.string1 = string;
+	}
+
+	public void updateString2(String string) 
+	{		
+		this.string2 = string;
+	}
+
+	public void setIconColor(Color color)
 	{
 		
 	}
