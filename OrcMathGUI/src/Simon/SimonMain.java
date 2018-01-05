dbe82493ccb3789bf1850e358fadb947996ff75d
a <a@localhost>
2018-01-04 19:20:42 -0500

    Yo

--- OrcMathGUI/src/Simon/SimonMain.java
+++ OrcMathGUI/src/Simon/SimonMain.java
@@ -3,21 +3,31 @@ package Simon;
 import java.util.ArrayList;
 
 import guiTeacher.GUIApplication;
+import holiday.Front;
+import holiday.HolidayCard;
 
 public class SimonMain extends GUIApplication 
 {
+	public static SimonMain game;
+	public static SimonScreen screen;
 	
+	public static void main(String[] args) {
+		game = new SimonMain(800, 500);
+		Thread runner = new Thread(game);
+		runner.start();
+	}
 	public SimonMain(int width, int height) 
 	{
 		super(width, height);
+		setVisible(true);
 		ArrayList<Move> moves = new ArrayList<Move>();
 	}
 
 	@Override
 	public void initScreen() 
 	{
-		
-
+		screen = new SimonScreen(getWidth(), getHeight());
+		setScreen(screen);
 	}
 
 }
--- OrcMathGUI/src/Simon/SimonScreen.java
+++ OrcMathGUI/src/Simon/SimonScreen.java
@@ -40,8 +40,24 @@ public class SimonScreen extends ClickableScreen
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) 
 	{
-		stat = new TextArea(140, 180, getWidth()-220, 400, "Current Round"+round+"/nSequenceLength"+sequenceLength);
+		stat = new TextArea(140, 180, getWidth()-220, 400, "Current Round "+round+"\nSequenceLength "+sequenceLength);
 		b1 = new CustomButton(50,50, 100, 100, "",Color.RED, new Action(){ public void act() { clicked(); } });
+		b2 = new CustomButton(50,50, 100, 100, "",Color.BLUE, new Action(){ public void act() { clicked(); } });
+		b3 = new CustomButton(50,50, 100, 100, "",Color.YELLOW, new Action(){ public void act() { clicked(); } });
+		b4 = new CustomButton(50,50, 100, 100, "",Color.GREEN, new Action(){ public void act() { clicked(); } });
+		b5 = new CustomButton(50,50, 100, 100, "",Color.PINK, new Action(){ public void act() { clicked(); } });
+		b6 = new CustomButton(50,50, 100, 100, "",Color.CYAN, new Action(){ public void act() { clicked(); } });
+		b7 = new CustomButton(50,50, 100, 100, "",Color.ORANGE, new Action(){ public void act() { clicked(); } });
+		b8 = new CustomButton(50,50, 100, 100, "",Color.BLACK, new Action(){ public void act() { clicked(); } });
+		viewObjects.add(stat);
+		viewObjects.add(b1);
+		viewObjects.add(b2);
+		viewObjects.add(b3);
+		viewObjects.add(b4);
+		viewObjects.add(b5);
+		viewObjects.add(b6);
+		viewObjects.add(b7);
+		viewObjects.add(b8);
 	}
 	private void clicked()
 	{
