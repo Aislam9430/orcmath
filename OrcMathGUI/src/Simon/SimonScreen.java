374597bec4001c51cdff559a5ad43edd8eb50e7b
a <a@localhost>
2018-01-04 20:56:20 -0500

    yO

--- OrcMathGUI/src/Simon/SimonScreen.java
+++ OrcMathGUI/src/Simon/SimonScreen.java
@@ -21,6 +21,7 @@ public class SimonScreen extends ClickableScreen
 	private CustomButton b7;
 	private CustomButton b8; 
 	private TextArea stat;	
+	private TextArea turn;
 	
 	private int round;
 	private int sequenceLength;
@@ -41,15 +42,17 @@ public class SimonScreen extends ClickableScreen
 	public void initAllObjects(List<Visible> viewObjects) 
 	{
 		stat = new TextArea(140, 180, getWidth()-220, 400, "Current Round "+round+"\nSequenceLength "+sequenceLength);
+		turn = new TextArea(175, 400, getWidth()-220, 400, "");
 		b1 = new CustomButton(50,50, 75, 75, "",Color.RED, new Action(){ public void act() { clicked(); } });
-		b2 = new CustomButton(325,325, 75, 75, "",Color.BLUE, new Action(){ public void act() { clicked(); } });
+		b2 = new CustomButton(300,300, 75, 75, "",Color.BLUE, new Action(){ public void act() { clicked(); } });
 		b3 = new CustomButton(175,50, 75, 75, "",Color.YELLOW, new Action(){ public void act() { clicked(); } });
 		b4 = new CustomButton(50,175, 75, 75, "",Color.GREEN, new Action(){ public void act() { clicked(); } });
-		b5 = new CustomButton(325,175, 75, 75, "",Color.PINK, new Action(){ public void act() { clicked(); } });
-		b6 = new CustomButton(175,325, 75, 75, "",Color.CYAN, new Action(){ public void act() { clicked(); } });
-		b7 = new CustomButton(325,50, 75, 75, "",Color.ORANGE, new Action(){ public void act() { clicked(); } });
-		b8 = new CustomButton(50,325, 75, 75, "",Color.BLACK, new Action(){ public void act() { clicked(); } });
+		b5 = new CustomButton(300,175, 75, 75, "",Color.PINK, new Action(){ public void act() { clicked(); } });
+		b6 = new CustomButton(175,300, 75, 75, "",Color.CYAN, new Action(){ public void act() { clicked(); } });
+		b7 = new CustomButton(300,50, 75, 75, "",Color.ORANGE, new Action(){ public void act() { clicked(); } });
+		b8 = new CustomButton(50,300, 75, 75, "",Color.BLACK, new Action(){ public void act() { clicked(); } });
 		viewObjects.add(stat);
+		viewObjects.add(turn);
 		viewObjects.add(b1);
 		viewObjects.add(b2);
 		viewObjects.add(b3);
@@ -59,9 +62,25 @@ public class SimonScreen extends ClickableScreen
 		viewObjects.add(b7);
 		viewObjects.add(b8);
 	}
+	private void playing()
+	{
+		turn.setText("Simon's Turn");
+		//light up buttons store info in arraylist
+		turn.setText("Your Turn");
+		pause(600);
+		turn.setText("");
+	}
 	private void clicked()
 	{
 		
+	}
+	public void pause(long time)
+	{
+		try {
+			Thread.sleep(time);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
